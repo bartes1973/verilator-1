62321a3e5c250618c2269379698ec055211c3709
Maciej Sobkowski <msobkowski@antmicro.com>
2020-04-21 23:49:22 +0200

    try copying insertBeforeStmt from V3Premit

--- src/V3Increments.cpp
+++ src/V3Increments.cpp
@@ -39,12 +39,67 @@ private:
     // STATE
     int m_modIncrementsNum;  // Var name counter
 
+    AstNodeModule* m_modp;  // Current module
+    AstCFunc* m_funcp;  // Current block
+    AstNode* m_stmtp;  // Current statement
+    AstWhile* m_inWhilep;  // Inside while loop, special statement additions
+    AstTraceInc* m_inTracep;  // Inside while loop, special statement additions
+    bool m_assignLhs;  // Inside assignment lhs, don't breakup extracts
+
+    bool m_noopt;  // Disable optimization of variables in this block
+
     // METHODS
     void insertBefore(AstNode* placep, AstNode* newp) {
         AstNRelinker linker;
         placep->unlinkFrBack(&linker);
         newp->addNext(placep);
         linker.relink(newp);
+    }
+
+    AstVar* getBlockTemp(AstNode* nodep) {
+        string newvarname = (string("__Vtemp") + cvtToStr(m_modp->varNumGetInc()));
+        AstVar* varp
+            = new AstVar(nodep->fileline(), AstVarType::STMTTEMP, newvarname, nodep->dtypep());
+        m_funcp->addInitsp(varp);
+        return varp;
+    }
+
+    void insertBeforeStmt(AstNode* newp) {
+        // Insert newp before m_stmtp
+        if (m_inWhilep) {
+            // Statements that are needed for the 'condition' in a while
+            // actually have to be put before & after the loop, since we
+            // can't do any statements in a while's (cond).
+            m_inWhilep->addPrecondsp(newp);
+        } else if (m_inTracep) {
+            m_inTracep->addPrecondsp(newp);
+        } else if (m_stmtp) {
+            AstNRelinker linker;
+            m_stmtp->unlinkFrBack(&linker);
+            newp->addNext(m_stmtp);
+            linker.relink(newp);
+        } else {
+            newp->v3fatalSrc("No statement insertion point.");
+        }
+    }
+
+    void createDeepTemp(AstNode* nodep, bool noSubst) {
+        if (debug() > 8) nodep->dumpTree(cout, "deepin:");
+
+        AstNRelinker linker;
+        nodep->unlinkFrBack(&linker);
+
+        AstVar* varp = getBlockTemp(nodep);
+        if (noSubst) varp->noSubst(true);  // Do not remove varrefs to this in V3Const
+        // Replace node tree with reference to var
+        AstVarRef* newp = new AstVarRef(nodep->fileline(), varp, false);
+        linker.relink(newp);
+        // Put assignment before the referencing statement
+        AstAssign* assp = new AstAssign(nodep->fileline(),
+                                        new AstVarRef(nodep->fileline(), varp, true), nodep);
+        insertBeforeStmt(assp);
+        if (debug() > 8) assp->dumpTree(cout, "deepou:");
+        nodep->user1(true);  // Don't add another assignment
     }
 
 public:
@@ -55,6 +110,62 @@ public:
     }
 
     virtual ~IncrementsVisitor() {}
+
+    bool noOpt() const { return m_noopt; }
+
+    void startStatement(AstNode* nodep) {
+        m_assignLhs = false;
+        if (m_funcp) m_stmtp = nodep;
+    }
+    virtual void visit(AstWhile* nodep) VL_OVERRIDE {
+        UINFO(4, "  WHILE  " << nodep << endl);
+        startStatement(nodep);
+        iterateAndNextNull(nodep->precondsp());
+        startStatement(nodep);
+        m_inWhilep = nodep;
+        iterateAndNextNull(nodep->condp());
+        m_inWhilep = NULL;
+        startStatement(nodep);
+        iterateAndNextNull(nodep->bodysp());
+        iterateAndNextNull(nodep->incsp());
+        m_stmtp = NULL;
+    }
+    virtual void visit(AstNodeAssign* nodep) VL_OVERRIDE {
+        startStatement(nodep);
+//XXX: Not sure if this is needed
+#if 0
+        {
+            bool noopt = IncrementsVisitor(nodep).noOpt();
+            if (noopt && !nodep->user1()) {
+                // Need to do this even if not wide, as e.g. a select may be on a wide operator
+                UINFO(4, "Deep temp for LHS/RHS\n");
+                createDeepTemp(nodep->rhsp(), false);
+            }
+        }
+#endif
+        iterateAndNextNull(nodep->rhsp());
+        m_assignLhs = true;
+        iterateAndNextNull(nodep->lhsp());
+        m_assignLhs = false;
+        m_stmtp = NULL;
+    }
+    virtual void visit(AstNodeStmt* nodep) VL_OVERRIDE {
+        if (!nodep->isStatement()) {
+            iterateChildren(nodep);
+            return;
+        }
+        UINFO(4, "  STMT  " << nodep << endl);
+        startStatement(nodep);
+        iterateChildren(nodep);
+        m_stmtp = NULL;
+    }
+    virtual void visit(AstTraceInc* nodep) VL_OVERRIDE {
+        startStatement(nodep);
+        m_inTracep = nodep;
+        iterateChildren(nodep);
+        m_inTracep = NULL;
+        m_stmtp = NULL;
+    }
 
     virtual void visit(AstPreAdd* nodep) VL_OVERRIDE {
         iterateChildren(nodep);
@@ -71,8 +182,7 @@ public:
         AstNode* replacep = backp->op1p();
         FileLine* fl = replacep->fileline();
 
-
-        insertBefore(backp->backp(),
+        insertBeforeStmt(//backp->backp(),
                      new AstAssign(fl, new AstVarRef(fl, vr->varp(), true),
                                    new AstAdd(fl, new AstVarRef(fl, vr->varp(), false),
                                               newconstp)));
